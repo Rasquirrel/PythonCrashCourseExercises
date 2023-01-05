216f7c84cfb10c90af1466077553b861e5173bfc
Isac <jose-0A4@protonmail.com>
2023-01-05 14:55:21 -0300

    Improved the code of toppings.py and creates and made the code of favorite_fruits.py

---
+++ favorite_fruits.py
@@ -0,0 +1,12 @@
+fruits = ['banana','orange','apple']
+
+if 'banana' in fruits:
+    print('Banana is monkey food, and I like it.')
+if 'grape' not in fruits:
+    print('I also like grapes but they are not in the list.')
+if 'orange' in fruits:
+    print('I really like orange juice')
+if 'apple' in fruits:
+    print('I like apples.')
+if 'banana' in fruits:
+    print('I really wanted to eat some bananas...')
--- toppings.py
+++ toppings.py
@@ -1,10 +1,16 @@
-requested_toppings = ['mushrooms', 'extra cheese']
+avaliable_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple',
+                      'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-    print('Adding mushrooms...')
-if 'pepperoni' in requested_toppings:
-    print('Adding pepperoni...')
-if 'extra cheese' in requested_toppings:
-    print('Adding extra cheese...')
+requested_toppings = ['mushrooms', 'olives', 'corns']
 
-print('\nFinished making your pizza!')
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in avaliable_toppings:
+            print('Adding ' + requested_topping + '...' )
+        else:
+            print('Sorry, we are out of ' + requested_topping + '. :(')
+
+    print('Finished making your pizza!')
+else:
+    print('Are you sure you want a plain pizza?')
+
