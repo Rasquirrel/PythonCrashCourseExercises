62223c4c3e2f9fc39c7f81573a97e325dfbc865c
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-07 15:52:21 -0300

    greeter.py - Bigger prompt message

--- greeter.py
+++ greeter.py
@@ -1,2 +1,8 @@
-name = input('Please, tell me your name: ')
+# This program will ask the user name and then print a greeting
+
+# Big message in the prompt variable:
+prompt = 'If you tell us your name, we can personalize the content you see.'
+prompt += '\nPlease, enter your name: '
+
+name = input(prompt)
 print('Hello, ' + name +'!')
