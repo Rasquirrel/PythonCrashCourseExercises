ff58a0e6334c21590089175283f6e0b49fadcd1e
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-07 14:23:47 -0300

    pets.py - Created the dictionarys

---
+++ pets.py
@@ -0,0 +1,27 @@
+dina = {
+    'type': 'cat',
+    'fur_color': 'white with brown',
+    'eyes_color': 'blue',
+    'size': 'medium',
+    'race': 'mixed'
+}
+
+newton = {
+    'type': 'cat',
+    'fur_color': 'white and black',
+    'eyes_color': 'yellow',
+    'size': 'small',
+    'race': 'mixed'
+}
+
+xane = {
+    'type': 'cat',
+    'fur_color': 'brown with white',
+    'eyes_color': 'blue',
+    'size': 'medium',
+    'race': 'mixed'
+}
+
+print(dina)
+print(xane)
+print(newton)
