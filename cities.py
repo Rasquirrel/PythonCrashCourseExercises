5411c50236482c6c614f0e1880911712b8cff555
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-07 15:01:49 -0300

    cities.py - Created the dictionaries inside the dictionaries

---
+++ cities.py
@@ -0,0 +1,24 @@
+cities = {
+    'sobral': {
+        'size': 'medium',
+        'country': 'brazil',
+        'language': 'portuguese',
+        'population': 210.711,
+        'curiosity': "It's called 'North Little Princess'."
+    },
+    'vancouver': {
+        'size': 'big',
+        'country': 'canada',
+        'language': 'english',
+        'population': 675.218,
+        'curiosity': "Vancouver has the 4th largest cruise ship terminal in the world."
+    },
+    'florence': {
+        'size': 'medium',
+        'country': 'italy',
+        'language': 'italian',
+        'population': 382.258,
+        'curiosity' : "It was the birthplace of the Renaissance." 
+    }
+}
+
