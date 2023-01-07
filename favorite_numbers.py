8815d42d87f9e7cbfc9aec16ec78724cb0ced659
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-07 14:49:12 -0300

    favorite_number.py - Finished the code

--- favorite_numbers.py
+++ favorite_numbers.py
@@ -1,18 +1,13 @@
 favorite_numbers = {
-                   'mae': 2,
-                   'isac': 4,
-                   'john': 7,
-                   'doe' : 6,
-                   'dina': 4
-                   }
+                   'mae': [2, 4, 5, 6],
+                   'isac': [4, 9, 13, 8],
+                   'john': [1, 2, 3, 99],
+                   'doe' : [999, 10, 20 ,30],
+                   'dina': [4, 9, 13, 8]
+                  }
 
-print("Mom's favorite number is "
- + str(favorite_numbers['mae']) + ';')
-print("Isac's favorite number is "
- + str(favorite_numbers['isac']) + ';')
-print("John Doe's favorite number is "
- + str(favorite_numbers['john']) + '.')
-print("Dina's favorite number is " 
- + str(favorite_numbers['dina']) + '.')
-print("Doe's favorite number is "
- + str(favorite_numbers['doe']) + '.' )
+for name, numbers in favorite_numbers.items():
+    print("\n" + name.title() + "'s favorite numbers are: ")
+    for number in numbers:
+        print("\t" + str(number) + ";")
+print('\n')
