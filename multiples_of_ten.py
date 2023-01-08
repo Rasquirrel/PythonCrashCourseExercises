929732237a69ba94274d6787ed814ce9ee2c6d74
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-08 15:54:02 -0300

    multiples_of_ten.py - Finished the code.

--- multiples_of_ten.py
+++ multiples_of_ten.py
@@ -2,6 +2,12 @@
 # it's a multiple of 10 or not.
 
 prompt = "Enter a number, and I will tell you if it's\n"
-prompt += "a multiple of 10"
+prompt += "a multiple of 10 or not: \n"
 
 number = input(prompt)
+number = int(number)
+
+if number % 10 == 0:
+    print("\nThe number  " + str(number) + " is multiple of 10.")
+else:
+    print("\nThe number " + str(number) + " isn't multiple of 10.")
