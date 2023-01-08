45da3ca1bfcac846cec7780ecc595ab28848602e
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-08 15:49:13 -0300

    multiples_of_ten - Added the prompt and renamed the file

---
+++ multiples_of_ten.py
@@ -0,0 +1,7 @@
+# This program will ask the user a number, and then tell if
+# it's a multiple of 10 or not.
+
+prompt = "Enter a number, and I will tell you if it's\n"
+prompt += "a multiple of 10"
+
+number = input(prompt)
