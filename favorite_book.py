52e79f66c01993472428efe443548fd143651915
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-10 13:34:09 -0300

    favorite_book.py - Finished

---
+++ favorite_book.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Prints a message telling that your favorite book is ..."""
+
+    print("My favorite book is " + title.title() + ".")
+
+favorite_book('python crash course')
