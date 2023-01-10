eb2bc55d9abcdf5e0185ca149c0923aebd7b11cf
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-10 13:49:54 -0300

    pets.py - Keyword Arguments

--- pets.py
+++ pets.py
@@ -6,3 +6,9 @@ def describe_pet(animal_type, pet_name):
 describe_pet('cat', 'dina')
 describe_pet('cat', 'newton')
 describe_pet('parrot', 'willam')
+print()
+# Keyword Arguments:
+
+describe_pet(animal_type='cat', pet_name='dina')
+describe_pet(pet_name='belu', animal_type='dog')
+describe_pet(animal_type='dog', pet_name='theo')
