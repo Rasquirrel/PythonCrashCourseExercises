276aea4c770eb477ed036614c2fb80a3bc4eded4
José Isac <96674887+Rasquirrel@users.noreply.github.com>
2023-01-10 13:55:18 -0300

    pets.py - Default Values and Funny Results

--- pets.py
+++ pets.py
@@ -1,4 +1,4 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='cat'):
     """Display informations about a pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
@@ -12,3 +12,9 @@ print()
 describe_pet(animal_type='cat', pet_name='dina')
 describe_pet(pet_name='belu', animal_type='dog')
 describe_pet(animal_type='dog', pet_name='theo')
+
+# Using Default Value
+
+describe_pet(pet_name='miranga')
+describe_pet('tina')
+describe_pet('leleco')
