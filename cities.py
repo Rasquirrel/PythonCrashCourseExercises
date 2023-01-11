a7b54977f047ca825d5e31387a2f10bb3727bc9d
Isac <jose-0A4@protonmail.com>
2023-01-11 13:00:23 -0300

    First two exercises from chapter eight

---
+++ cities.py
@@ -0,0 +1,7 @@
+def describe_city(name, country='brasil'):
+    print(name.title() + " is located in " + country.title() + ".\n")
+
+describe_city('florence', country='italy')
+describe_city('paris', country='france')
+describe_city('washingtom', country='united states of america')
+describe_city('sobral')
---
+++ t_shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(message, size="g"):
+    print("Done t-shirt size " + size.upper() + " whith\n" +
+    "the message '" + message + "' stamped.")
+    
+make_shirt("Ashagg")
+make_shirt("fiduyy")
