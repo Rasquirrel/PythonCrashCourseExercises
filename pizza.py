ed59f4d909b6f848186740a9735584bc869e69b5
José Isac <jose-0A4@protonmail.com>
2023-01-13 18:00:36 -0300

    pizza.py

---
+++ pizza.py
@@ -0,0 +1,7 @@
+def make_pizza(*toppings):
+    """This function prints the list of topping that have been requested"""
+    print(toppings)
+
+
+make_pizza('cheese')
+make_pizza('onions', 'peppers', 'mushrooms')
