a280b8ac5ac5b52919f75a0b42a74622ad768d16
José Isac <jose-0A4@protonmail.com>
2023-01-13 17:37:38 -0300

    magicians.py

---
+++ magicians.py
@@ -0,0 +1,9 @@
+magicians = ['pablo', 'tim', 'franchesco']
+
+
+def show_magicians(magicians_list: list):
+    for magician in magicians_list:
+        print(magician.title())
+
+
+show_magicians(magicians)
