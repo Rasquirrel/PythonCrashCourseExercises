2258ab2c49508ce3abddeda2c37d731b7fec71c1
José Isac <jose-0A4@protonmail.com>
2023-01-14 13:45:09 -0300

    learning about modules

--- pizza.py
+++ pizza.py
@@ -1,9 +1,5 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """This function prints the list of topping that have been requested"""
-    print("Making a pizza with the followind toppings: ")
+    print("Making a " + str(size) + "-inch pizza with the following toppings:")
     for topping in toppings:
         print('\t' + topping + ';')
-
-
-make_pizza('cheese')
-make_pizza('onions', 'peppers', 'mushrooms')
