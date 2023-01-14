2f12c2b2639013f209b8ace18bd6af9a9e797b73
José Isac <jose-0A4@protonmail.com>
2023-01-14 18:18:27 -0300

    More exercises from chapter 9

---
+++ car.py
@@ -0,0 +1,37 @@
+class Car:
+    """A simple attempt to represent a car"""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage"""
+        print('This car has ' + str(self.odometer_reading) + ' miles on it')
+
+    def update_odometer(self, mileage: int):
+        """Set the odometer reading to the given value"""
+        if mileage <= self.odometer_reading:
+            print('Hey, you can not rollback a odometer!')
+        else:
+            self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        """Add the given value to the odometer reading"""
+        self.odometer_reading += miles
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(50)
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
