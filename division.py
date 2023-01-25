c32d294f0d442b95f94f2bed6e79237e64d1694c
Isac <jose-0A4@protonmail.com>
2023-01-25 07:17:01 -0300

    division.py - Finished

--- division.py
+++ division.py
@@ -1,5 +1,17 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print('Você não pode dividir por zero!')
+print("Give me two numbers, I'll divide them.")
+print("Enter 'q' to exit.")
+
+while True:
+    first_number = input('First Number: ')
+    if first_number == "q": break
+
+    second_number = input('Second Number: ')
+    if second_number == 'q': break
+
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError: 
+        print("You can't divide a number by 0!")
+    else:
+        print(answer)
 
